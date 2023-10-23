3e06d3bbe376178cd2d8fd05e6b55e94036c549d
Mahbub2027 <alam201127@gmail.com>
2023-10-24 00:42:04 +0600

    first commit

---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+           
+            <h2>This is Home page</h2>
+        </div>
+    );
+};
+
+export default Home;
